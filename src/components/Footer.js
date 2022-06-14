1b92d2d96d26fb581db925ada09ca8fc3e808101
Claudia Chittim <cchittim10101@gmail.com>
2022-06-14 12:40:34 -0500

    added to footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a
+            href="https://github.com/c0y01202/React-Portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Claudia Chittim
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com/in/cchittim/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>{" "}
+          |{" "}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
